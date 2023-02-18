a063f18dcc39c561c782e845430ca37a7649661a
kujtimsadiku <kujtim_sadiku@hotmail.com>
2023-02-19 00:29:44 +0200

    update

--- ft_printnum.c
+++ ft_printnum.c
@@ -1,22 +1,5 @@
 #include "ft_printf.h"
 
-/*
-	0. Build number
-	1. Minus from length the lenght of string
-	2. Minus from length: sign and prefix
-	3. Print spaces for length
-	4. Print sign (comes from %d %i) print_signed(){	sign = - / +	}
-	5. Print prefix
-	6. Print zeros
-	7. Print number from build num
-	8. Print ladjust spaces
-*/
-
-
-/*
-	doesn't print the - values.
-*/
-
 int	ft_printulong(t_data *data, uintptr_t u, int base)
 {
 	static char	digits[MAXBUF];
